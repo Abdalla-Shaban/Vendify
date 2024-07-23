8d76555c141b946446500eeaa7e23b3b9de47b02
abdalla <abdallashaban584@gmail.com>
2024-07-23 17:09:16 +0300

    fix layout style

--- client/app/(root)/layout.tsx
+++ client/app/(root)/layout.tsx
@@ -4,10 +4,10 @@ import UpButton from "@/components/UpButton";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="relative container">
+    <main className="relative container flex flex-col min-h-screen">
       <UpButton />
       <Navbar />
-      {children}
+      <div className="flex-1">{children}</div>
       <Footer />
     </main>
   );
