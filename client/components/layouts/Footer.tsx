905aebed4c3df3a7a40b8da8a9e539013207eacb
abdalla <abdallashaban584@gmail.com>
2024-07-26 00:25:15 +0300

    responsive Navbar and Footer

--- client/components/layouts/Footer.tsx
+++ client/components/layouts/Footer.tsx
@@ -4,17 +4,24 @@ import Link from "next/link";
 const Footer = () => {
   const t = useTranslations("Footer");
   return (
-    <footer className="flex items-center justify-between py-5 border-t-2">
-      <div className="flex items-center gap-4">
+    <footer className="flex flex-col xl:flex-row md:items-center justify-center md:justify-between py-5 border-t-2">
+      <div className="flex flex-col xl:flex-row items-center gap-4">
         <h2 className="text-24 font-bold">Vendify</h2>
-        <div className="flex items-center gap-4 text-14">
-          <Link href="">{t("contact-us")}</Link>
-          <Link href="">{t("privacy-policy")}</Link>
-          <Link href="">{t("terms-of-use")}</Link>
-          <Link href="">{t("refund-cancellation-policy")}</Link>
+        <div className="flex flex-col xl:flex-row items-center gap-4 text-14">
+          <Link
+            href="/contact-us"
+            className="hover:underline hover:text-green-600"
+          >
+            {t("contact-us")}
+          </Link>
+          <div>{t("privacy-policy")}</div>
+          <div>{t("terms-of-use")}</div>
+          <div>{t("refund-cancellation-policy")}</div>
         </div>
       </div>
-      <div>Copyright &copy; 2024 Vendify Program. All rights reserved</div>
+      <div className="text-center mt-3 xl:mt-0">
+        Copyright &copy; 2024 Vendify Program. All rights reserved
+      </div>
     </footer>
   );
 };
--- client/components/layouts/Navbar.tsx
+++ client/components/layouts/Navbar.tsx
@@ -1,26 +1,58 @@
+"use client";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
 import LocaleSwitcher from "../LocaleSwitcher";
 import { NavbarLinks } from "@/constants";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { Menu } from "lucide-react";
 
 const Navbar = () => {
   const t = useTranslations("Navbar");
+
   return (
-    <nav className="flex items-center justify-between p-3 border-b-2">
-      <h1 className="text-24 font-semibold">Vendify</h1>
-      <ul className="flex items-center gap-5 text-16">
-        <li className="duration-300 hover:text-green-950 hover:font-bold">
+    <nav className="flex items-center justify-between border-b-2">
+      <h1 className="text-28 font-bold text-green-950">
+        <Link href="/">Vendify</Link>
+      </h1>
+      <div className="hidden md:flex items-center space-x-1 text-16">
+        <div className="duration-300 hover:text-green-600 border-b-2 border-b-transparent hover:border-green-600 p-4">
           <LocaleSwitcher />
-        </li>
+        </div>
         {NavbarLinks.map(({ title, href }) => (
-          <li
+          <Link
             key={href}
-            className="duration-300 hover:text-green-950 hover:font-bold"
+            className="duration-300 hover:text-green-600 border-b-2 border-b-transparent hover:border-green-600 p-4"
+            href={href}
           >
-            <Link href={href}>{t(title)}</Link>
-          </li>
+            {t(title)}
+          </Link>
         ))}
-      </ul>
+      </div>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button className="bg-green-950 text-white my-3 md:hidden" isIconOnly>
+            <Menu />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="menu links" items={NavbarLinks}>
+          {({ title, href }) => (
+            <DropdownItem
+              key={href}
+              textValue={title}
+              href={href}
+              className="p-4"
+            >
+              {t(title)}
+            </DropdownItem>
+          )}
+        </DropdownMenu>
+      </Dropdown>
     </nav>
   );
 };
