905e78568c8409fe136a59097cf79d695dd64fd4
abdalla <abdallashaban584@gmail.com>
2024-07-29 01:21:37 +0300

    add dashboard metadata

--- client/app/(root)/affiliate/dashboard/page.tsx
+++ client/app/(root)/affiliate/dashboard/page.tsx
@@ -1,4 +1,13 @@
 import Statistics from "@/components/sections/Statistics";
+import { Locale } from "@/config";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(Locale: Locale) {
+  const t = await getTranslations({ Locale, namespace: "Metadata" });
+  return {
+    title: t("dashboard"),
+  };
+}
 
 const DashboardPage = () => {
   return (
