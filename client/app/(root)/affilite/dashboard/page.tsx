0f329deeebed652aeb2610c977251c2bce1265bd
abdalla <abdallashaban584@gmail.com>
2024-07-29 00:52:57 +0300

    implement dashboard page

---
+++ client/app/(root)/affilite/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Statistics from "@/components/sections/Statistics";
+
+const DashboardPage = () => {
+  return (
+    <div className="flex-1 mb-8">
+      <Statistics showLink={false} />
+    </div>
+  );
+};
+
+export default DashboardPage;
