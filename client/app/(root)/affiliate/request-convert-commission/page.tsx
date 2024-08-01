9205969fc48c937ca6a7d9799a83ab9948d7a9ae
abdalla <abdallashaban584@gmail.com>
2024-08-02 01:51:47 +0300

    implement request convert commission page

---
+++ client/app/(root)/affiliate/request-convert-commission/page.tsx
@@ -0,0 +1,21 @@
+import { Locale } from "@/config";
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(Locale: Locale) {
+  const t = await getTranslations({ Locale, namespace: "Metadata" });
+  return {
+    title: t("convertCommission"),
+  };
+}
+
+function RequestCovertCommissionPage() {
+  const t = useTranslations("ConvertCommissionPage");
+  return (
+    <div className="flex-1 flex items-center justify-center">
+      <h1 className="text-32 font-bold">{t("title")}</h1>
+    </div>
+  );
+}
+
+export default RequestCovertCommissionPage;
