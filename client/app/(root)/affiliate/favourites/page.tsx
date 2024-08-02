5369bd6e600823b248b07ac8da943e9d0766b17a
abdalla <abdallashaban584@gmail.com>
2024-08-02 17:16:47 +0300

    implement favourites page

---
+++ client/app/(root)/affiliate/favourites/page.tsx
@@ -0,0 +1,24 @@
+import { Locale } from "@/config";
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata(Locale: Locale) {
+  const t = await getTranslations({ Locale, namespace: "Metadata" });
+  return {
+    title: t("favourites"),
+  };
+}
+
+function FavouritesPage() {
+  const t = useTranslations("FavouritesPage");
+  return (
+    <div className="flex-1 my-8 p-5 shadow-xl rounded-lg">
+      <h1 className="text-32 font-bold">{t("title")}</h1>
+      <div className="flex items-start justify-center text-28 font-semibolb">
+        <div className="mt-5">{t("emptyContent")}</div>
+      </div>
+    </div>
+  );
+}
+
+export default FavouritesPage;
